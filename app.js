492725ba00f24f5d127b2c54172b9b88600d157b
Michael Washburn <washburn.michael@gmail.com>
2017-12-09 11:25:51 -0800

    added app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+//app.js is the entry point for application. Index.js is also used
+
+// Here we are importing modules that we have installed
+
+const express = require ('express');
+
+const app = express();
+
+// Index Route
+app.get('/', (req, res) => {
+  res.send('Index');
+});
+
+// About Route
+app.get('/about', (req, res) => {
+  res.send('ABOUT');
+})
+
+const port = 5000;
+
+app.listen(port, () =>{
+  console.log(`Server started on port ${port}`) //ES6
+  //above is the same as writing console.log('Server started on port ' + port);
+});
