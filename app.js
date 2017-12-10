6147e316f52badc1331ffa6012bd7c62be1f0d29
Michael Washburn <washburn.michael@gmail.com>
2017-12-09 16:10:40 -0800

    completed server side validation with body parser

--- app.js
+++ app.js
@@ -4,21 +4,33 @@
 
 const express = require ('express');
 const exphbs = require ('express-handlebars');
+const bodyParser = require ('body-parser');
 const mongoose = require ('mongoose');
 
 const app = express();
 
 // Map global promise - gets rid of warning
 mongoose.Promise = global.Promise;
+
+// Load Idea model
+require ('./models/Idea');
+const Idea = mongoose.model('ideas');
+
 // Connect to mongoose
 mongoose.connect('mongodb://localhost/vidjot-dev', {
   useMongoClient: true
 })
 .then(() => console.log('MongoDB Connected...')) //this catches a promise
 .catch(err => console.log(err));
+
+
 // Handlebars middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 
 
@@ -34,6 +46,37 @@ app.get('/', (req, res) => {
 // About Route
 app.get('/about', (req, res) => {
   res.render('about');
+});
+
+// Add Idea Form
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add');
+});
+
+// Process Idea form
+app.post('/ideas', (req, res) =>{
+  // Manually adding validations to the "Add Idea form"
+
+  /*  create an errors variable with an empty array. This array will collect errors when form fields are not filled in.*/
+  let errors = [];
+  /* Conditional functions where if a title or details fields are not filled in then we push an error to the errors array*/
+  if(!req.body.title){
+    errors.push({text: 'Please add a title'});
+  }
+  if(!req.body.details){
+    errors.push({text: 'Please add some details'});
+  }
+
+  // Check errors length
+  if(errors.length > 0){
+    res.render('ideas/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    res.send('passed');
+  }
 })
 
 const port = 5000;
